cad78ce4baa047d4836dcbda2d741f164f8f4fa8
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-05-02 17:52:38 +0100

    [tests] Make function 'read matrix' for both the ESS and param list

--- tests/test_AMIS_five_iters.R
+++ tests/test_AMIS_five_iters.R
@@ -20,20 +20,16 @@ expect_equal(beta_iter_2, expected_beta_iter_2)
 ### Test Effective Sample Size (ESS) and content of `param` matrix
 ### for first two iterations of AMIS
 
-read_matrix <- function(iter, prefix) {
+read_matrix <- function(iter, prefix, result) {
     filename <- paste(prefix, sprintf("_iteration_%g.csv", iter), sep = "")
     matrix(scan(filename),
-           nrow = dim(list_of_ESS[[iter]])[1],
-           ncol = dim(list_of_ESS[[iter]])[2],
+           nrow = dim(result[[iter]])[1],
+           ncol = dim(result[[iter]])[2],
            byrow = TRUE)
 }
 
-expected_ESS <- lapply(1:5, read_matrix, "./tests/test_data/ESS")
-expected_param <- lapply(1:5, read_matrix, "./tests/test_data/param")
-
-t <- 1
-expect_equal(expected_ESS[[1]], list_of_ESS[[t]])
-expect_equal(expected_param[[1]], list_of_param[[t]][,1:3])
+expected_ESS <- lapply(1:5, read_matrix, "./tests/test_data/ESS", list_of_ESS)
+expected_param <- lapply(1:5, read_matrix, "./tests/test_data/param", list_of_params)
 
 for (t in 1:5) {
     TRUE
