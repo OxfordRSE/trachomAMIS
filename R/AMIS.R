0d8bfdc3e8f9e8eb192382fea5d6063712af6224
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-05-05 16:00:59 +0100

    Don't specify package namespace in amis function

--- R/AMIS.R
+++ R/AMIS.R
@@ -2,9 +2,9 @@
 amis <- function(prevalence_map, transmission_model, n_params, nsamples, IO_file_id, delta = 5, T = 100, target_ess = 250) {
     param <- get_initial_parameters(nsamples)
     id <- function(t) paste(IO_file_id, sprintf("_it%g", t), sep = "")
-    simulated_prevalences <- trachomAMIS::run_transmission_model(seeds = 1:nsamples, parameters = param[,1], id(1))
-    WW <- trachomAMIS::compute_weight_matrix(prev, simulated_prevalences, delta, first_weight = rep(1, nsamples))
-    ess <- trachomAMIS::calculate_ess(WW)
+    simulated_prevalences <- run_transmission_model(seeds = 1:nsamples, parameters = param[,1], id(1))
+    WW <- compute_weight_matrix(prev, simulated_prevalences, delta, first_weight = rep(1, nsamples))
+    ess <- calculate_ess(WW)
     cat( min(ess),  "", max(ess), "\n")
 
     components <- list(GG = c(0),
@@ -22,10 +22,10 @@ amis <- function(prevalence_map, transmission_model, n_params, nsamples, IO_file
         param <- rbind(param, sample_new_parameters(clustMix, nsamples, prop$r))
         simulated_prevalences <- append(simulated_prevalences,
                                         run_transmission_model(seeds(t) , param[,1], id(t)))
-        components <- trachomAMIS::update_mixture_components(clustMix, components, t)
-        first_weight <- trachomAMIS::compute_prior_proposal_ratio(components, param, prop$d)
-        WW <- trachomAMIS::compute_weight_matrix(prev, simulated_prevalences, delta, first_weight)
-        ess <- trachomAMIS::calculate_ess(WW)
+        components <- update_mixture_components(clustMix, components, t)
+        first_weight <- compute_prior_proposal_ratio(components, param, prop$d)
+        WW <- compute_weight_matrix(prev, simulated_prevalences, delta, first_weight)
+        ess <- calculate_ess(WW)
         cat( c("min(ESS)=", min(ess),  ", max(ESS)=", max(ess), "\n"))
         if(min(ess) >= target_ess) break
     }
