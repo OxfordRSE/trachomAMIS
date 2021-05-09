0647b1fee541a6f7d8721ac4fbbe0c54cae88b70
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-05-09 15:57:14 +0100

    Add tests that compare ecdfs between data and sampled weights

---
+++ tests/test_ecdf.R
@@ -0,0 +1,34 @@
+devtools::load_all()
+
+sample_prevalence_map_at_IUs <- function(IU_indices, n.map.sampl, scenario_id) {
+    prev = matrix(NA, ncol = n.map.sampl, nrow = length(IU_indices))
+    sample_map <- function(IU_index) {
+        set.seed(scenario_id) # For comparison with test data with `set.seed(Scen[iscen])`
+        rnorm(n.map.sampl, Data$Logit[IU_index], sd = Data$Sds[IU_index])
+    }
+    L <- lapply(IU_indices, sample_map)
+    prev <- sapply(L, function(x) exp(x)/(1+exp(x)))
+
+    return(
+        t(prev*100)
+    )
+}
+
+library(tmvtnorm)
+library(mnormt)
+library(mclust)
+
+iscen <- 1
+scenario_id <- 36
+group_id <- 2
+Data = read.csv("./data/FinalDataPrev.csv")
+IU_scen <- which(Data$Scenario == scenario_id & Data$Group == group_id)
+prev <- sample_prevalence_map_at_IUs(IU_scen, n.map.sampl = 3000, scenario_id)
+
+############## Run AMIS ############
+T <- 5
+N<-rep(100,T)
+param_and_weights <- trachomAMIS::amis(prevalence_map = prev, transmission_model = NULL, n_params = 2, nsamples = 100, IO_file_id = sprintf("scen%g_group%g",  scenario_id,  group_id), delta = 5, T = T, target_ess = 250)
+
+source("tests/qa_ecdf_funcs.R")
+qa_plots(param_and_weights, prev, "tests/plots", scenario_id, group_id)
